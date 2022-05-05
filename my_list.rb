8cc7836c58d12ba002c523bd26932bfb6531a4da
rdnrn <rodina.arina00@gmail.com>
2022-05-05 14:38:07 +0300

    Add each method

--- my_list.rb
+++ my_list.rb
@@ -2,9 +2,17 @@ require_relative 'enumerable'
 
 class MyList
   include MyEnumerable
+
   def initialize(*list)
     @list = list
   end
+
+  def each
+    (0...@list.length).each do |i|
+      yield @list[i]
+    end
+  end
+
 end
 
 # Verify solution:
