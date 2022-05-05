43282b36b1259387fa2f55eee1ec7828c88ac4fb
rdnrn <rodina.arina00@gmail.com>
2022-05-05 11:25:28 +0300

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -9,18 +9,18 @@ end
 
 # Verify solution:
 # Create our list
-puts list = MyList.new(1, 2, 3, 4)
+list = MyList.new(1, 2, 3, 4)
 # <MyList: @list=[1, 2, 3, 4]>
 # Test #all?
-puts list.my_all? { |e| e < 5 }
+list.my_all? { |e| e < 5 }
 # true
-puts list.my_all? { |e| e > 5 }
+list.my_all? { |e| e > 5 }
 # false
 # Test #any?
-puts list.my_any? { |e| e == 2 }
+list.my_any? { |e| e == 2 }
 # true
-puts list.my_any? { |e| e == 5 }
+list.my_any? { |e| e == 5 }
 # false
 # Test #filter
-puts list.my_filter?(&:even?)
+list.my_filter?(&:even?)
 # [2, 4]
